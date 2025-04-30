08052433cc1d2b59ae7fdf2c15e144126012def0
francisco2435 <francisco.riquelme.n@usach.cl>
2025-04-29 23:39:23 -0400

    Creación Consulta SQL compleja 1

--- Backend/src/main/java/delivery/demo/Controllers/ClienteController.java
+++ Backend/src/main/java/delivery/demo/Controllers/ClienteController.java
@@ -1,6 +1,9 @@
 package delivery.demo.Controllers;
 
+import delivery.demo.Entities.ClienteEntity;
+import delivery.demo.Services.ClienteService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 
 public class ClienteController {
+    ClienteService clienteService;
+
+    @GetMapping("/con-mas-gasto")
+    public ClienteEntity obtenerClienteConMasGasto() {
+        return clienteService.obtenerClienteConMasGasto();
+    }
 }
--- Backend/src/main/java/delivery/demo/Services/ClienteService.java
+++ Backend/src/main/java/delivery/demo/Services/ClienteService.java
@@ -1,8 +1,18 @@
 package delivery.demo.Services;
 
+import delivery.demo.Entities.ClienteEntity;
+import delivery.demo.Repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    public ClienteEntity obtenerClienteConMasGasto() {
+        return clienteRepository.findClienteQueMasGasto();
+    }
 }
