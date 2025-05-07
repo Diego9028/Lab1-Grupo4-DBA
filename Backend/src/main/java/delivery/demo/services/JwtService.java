f446e8735ece86d285c32ec4239f9da2cedd0bb5
Duvan Vergara <duvan.vergara@usach.cl>
2025-05-07 19:01:46 -0400

    Alteracion para que funcione

--- Backend/src/main/java/delivery/demo/services/JwtService.java
+++ Backend/src/main/java/delivery/demo/services/JwtService.java
@@ -3,13 +3,11 @@ package delivery.demo.services;
 import delivery.demo.entities.ClienteEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Map;
 
