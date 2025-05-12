6b9424f2d6a4a4e0e466afd15209d1c3af01d3b1
Diego <diegomolinaarteaga@gmail.com>
2025-05-11 21:25:34 -0400

    Crud calificacion

--- Backend/src/main/java/delivery/demo/entities/CalificacionEntity.java
+++ Backend/src/main/java/delivery/demo/entities/CalificacionEntity.java
@@ -4,14 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CalificacionEntity {
 
-    private Long id;
-
-    private int total_puntos;
-    private Long idRepartidor;
-
+    private Integer idCalificacion;   // id_calificacion
+    private Integer totalPuntos;      // total_puntos
+    private Integer totalPedidos;     // total_pedidos
+    private BigDecimal promedio;      // promedio (NUMERIC)
+    private Integer idRepartidor;     // id_repartidor
 }
